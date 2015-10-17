bfa767a2aeaf24d42ca601a5079ccf1950762e21
Ania <malaania@malaania-NC10-N110.(none)>
2015-10-17 16:11:43 +0200

    dopisane liczby Fibonacciego

--- lista2_zad3.py
+++ lista2_zad3.py
@@ -1,5 +1,6 @@
 __author__ = 'malaania'
 import time
+import timeit
 
 class Licz():
     @classmethod
@@ -25,63 +26,52 @@ class Licz():
 
     @classmethod
     def fib_rek(cls, n):
-        if n==0:
-            return 0
-        elif n==1:
-            return 1
+        if n==0 or n==1:
+            return n
         else:
             return Licz.fib_rek(n-1)+Licz.fib_rek(n-2)
 
     @classmethod
-    def fib_wyj(cls, n):
-        if n==0:
-            raise Exception(0)
-        i=0
-        przedost = 0
-        wynik=1
-        while i<(n-1):
-            i+=1
-            temp_wynik=wynik
-            wynik +=przedost
-            przedost=temp_wynik
-        raise Exception(wynik)
+    def fib_wyj(cls,n):
+        if n == 0:
+            return 0
+        else:
+            try:
+                Licz.fib_acc(0,1,n)
+            except Exception as e:
+                return e.message
+
+    @classmethod
+    def fib_acc(cls,prev,curr,n):
+        if n==1:
+            raise Exception(curr)
+        else:
+            Licz.fib_acc( curr, curr+prev,n-1)
 
 print Licz.silnia_wyj(5)
 print Licz.silnia_rek(5)
-#print Licz.silnia_rek(5)
-
-#try:
-#    print Licz.silnia_wyj(5)
-#except Exception as e:
-#    print e.message
-#t_przed_rek = time.time()
-#Licz.silnia_rek(700)
-#t_po_rek = time.time()
-#print "silnia rekurencyjnie:"
-#print t_po_rek-t_przed_rek
-#t_przed_wyj = time.time()
-#try:
-#    print Licz.silnia_wyj(700)
-#except Exception as e:
-#    pass
-#t_po_wyj = time.time()
-#print "silnia z wyjatkiem:"
-#print t_po_wyj - t_przed_wyj
-#print "Liczby Fibonacciego rekurencyjnie:"
-#t_przed_rek = time.time()
-#Licz.fib_rek(20)
-#t_po_rek = time.time()
-#print(t_po_rek-t_przed_rek)
-#t_przed_wyj = time.time()
-#try:
-#    print Licz.fib_wyj(200)
-#except Exception as e:
-#    pass
-#t_po_wyj = time.time()
-#print "Liczby Fibonacciego z wyjatkami:"
-#print(t_po_wyj-t_przed_wyj)
-
-#Licz.silnia(5)
+print Licz.fib_rek(7)
+print Licz.fib_wyj(7)
+t_przed = time.time()
+for i in range(0,1000):
+    Licz.silnia_rek(50)
+t_po = time.time()
+print "Silnia rekurencyjna: " + (t_po-t_przed).__str__()
+t_przed = time.time()
+for i in range(0,1000):
+    Licz.silnia_wyj(50)
+t_po = time.time()
+print "Silnia z wyjatkiem: " + (t_po-t_przed).__str__()
+t_przed = time.time()
+for i in range(0,1000):
+    Licz.fib_rek(10)
+t_po = time.time()
+print "Fibonacci rekurencyjnie: " + (t_po-t_przed).__str__()
+t_przed = time.time()
+for i in range(0,1000):
+    Licz.fib_wyj(10)
+t_po = time.time()
+print "Fibonacci z wyjatkiem: " + (t_po-t_przed).__str__()
 
 
 
