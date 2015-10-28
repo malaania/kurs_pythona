ce28a901b4d6d35881df2ced18d574d87b3a3c47
Ania <malaania@malaania-NC10-N110.(none)>
2015-10-28 15:13:12 +0100

    do sth

--- lista3_zad2_2.py
+++ lista3_zad2_2.py
@@ -6,7 +6,18 @@ def pierwsze_skladana(n):
     pierwsze = [pierwsza for pierwsza in range(2, n) if pierwsza not in zlozone]
     return pierwsze
 
+def jest_pierwsza(n):
+    return not bool(filter(lambda i: n%i == 0, range(2,n)))
+def pierwsze_funkcyjna(n):
+    return filter(jest_pierwsza,range(2,n))
+
+print str(pierwsze_skladana(25))
+print str(pierwsze_funkcyjna(25))
 start = time.time()
-pierwsze_skladana(1000)
+pierwsze_skladana(2000)
 stop = time.time()
-print stop - start
+print "Lista skladana: "+ str(stop - start)
+start = time.time()
+pierwsze_funkcyjna(2000)
+stop = time.time()
+print "Lista funkcyjna: "+ str(stop - start)
