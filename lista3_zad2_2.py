5c91fe2faad0d71c081d07fb42dafa2f57b93351
Ania <malaania@malaania-NC10-N110.(none)>
2015-10-28 16:58:36 +0100

    do some changes

--- lista3_zad2_2.py
+++ lista3_zad2_2.py
@@ -1,6 +1,25 @@
 __author__ = 'malaania'
+import time
 
 def doskonale_skladana(n):
-    return [i for i in range(2, n) if sum([j for j in range(1, i/ 2 + 1) if i % j == 0]) == i]
+    return [i for i in range(2, n)
+            if sum([j for j in range(1, i / 2 + 1) if i % j == 0]) == i]
+
+
+def doskonale_funkcyjna(n):
+    return filter(lambda n: sum(i for i in xrange(1, n) if n % i == 0) == n,
+                  range(2, n))
+
 
 print str(doskonale_skladana(1000))
+print str(doskonale_funkcyjna(1000))
+start = time.time()
+doskonale_skladana(3000)
+stop = time.time()
+print "Lista skladana: "+ str(stop - start)
+start = time.time()
+doskonale_funkcyjna(3000)
+stop = time.time()
+print "Lista funkcyjna: "+ str(stop - start)
+
+
